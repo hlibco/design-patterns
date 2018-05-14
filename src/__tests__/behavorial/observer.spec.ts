c9343c7d8cb91f0070dfa3784bff8c2afde37bd4
Felipe Hlibco <hlibco@gmail.com>
2018-05-13 20:07:34 -0700

    feat(behavorial): add dp: observer

---
+++ src/__tests__/behavorial/observer.spec.ts
@@ -0,0 +1,24 @@
+import { Product, Markup, Taxes } from '../../behavorial/observer'
+
+describe('observer', () => {
+	test('sanity', () => {
+		const product = new Product()
+		const markup = new Markup()
+		const taxes = new Taxes()
+
+		product.register(markup)
+		product.register(taxes)
+
+		product.setBasePrice(100)
+		const price1 = product.getPrice()
+		const expectPrice1 = 100 * 1.3 * 1.09
+
+		product.unregister(taxes)
+		product.setBasePrice(100)
+		const price2 = product.getPrice()
+		const expectPrice2 = 100 * 1.3
+
+		expect(price1).toBe(expectPrice1)
+		expect(price2).toBe(expectPrice2)
+	})
+})
---
+++ src/behavorial/observer.ts
@@ -0,0 +1,39 @@
+class Product {
+	private price = 0
+	public actions = [] as any[]
+
+	setBasePrice(price: number) {
+		this.price = price
+		this.notifyAll()
+	}
+
+	getPrice() {
+		return this.price
+	}
+
+	register(observer) {
+		this.actions.push(observer)
+	}
+
+	unregister(observer) {
+		this.actions = this.actions.filter(el => el !== observer)
+	}
+
+	notifyAll() {
+		return this.actions.forEach(el => el.update(this))
+	}
+}
+
+class Markup {
+	update(product) {
+		product.price = product.price * (1 + 0.3)
+	}
+}
+
+class Taxes {
+	update(product) {
+		product.price = product.price * (1 + 0.09)
+	}
+}
+
+export { Product, Markup, Taxes }
