f9040066a8412591e17fb7d1f8a38f9674ba2195
Felipe Hlibco <hlibco@gmail.com>
2018-05-13 20:34:30 -0700

    feat(behavorial): add dp: state

---
+++ src/__tests__/behavorial/state.spec.ts
@@ -0,0 +1,18 @@
+import Order from '../../behavorial/state'
+
+describe('state', () => {
+	test('sanity', () => {
+		const order = new Order()
+		const label1 = order.getStateLabel()
+
+		order.nextState()
+		const label2 = order.getStateLabel()
+
+		order.nextState()
+		const label3 = order.getStateLabel()
+
+		expect(label1).toBe('waitingForPayment')
+		expect(label2).toBe('shipping')
+		expect(label3).toBe('delivered')
+	})
+})
---
+++ src/behavorial/state.ts
@@ -0,0 +1,43 @@
+class Order {
+	private state = new WaitingForPayment()
+
+	nextState() {
+		this.state = this.state.next()
+	}
+
+	getStateLabel() {
+		return this.state.getLabel()
+	}
+}
+
+class OrderStatus {
+	constructor(private label: string, private nextStatus) {}
+
+	next() {
+		return new this.nextStatus()
+	}
+
+	getLabel() {
+		return this.label
+	}
+}
+
+class WaitingForPayment extends OrderStatus {
+	constructor() {
+		super('waitingForPayment', Shipping)
+	}
+}
+
+class Shipping extends OrderStatus {
+	constructor() {
+		super('shipping', Delivered)
+	}
+}
+
+class Delivered extends OrderStatus {
+	constructor() {
+		super('delivered', Delivered)
+	}
+}
+
+export default Order
